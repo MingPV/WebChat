ef62ed27d422a3fe883a695c0de6dc2c6e943aa5
Bhanarin-Han <pinyapach.h03@gmail.com>
2025-04-17 13:46:23 +0700

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,27 +3,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-24 dark:bg-gray-900">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-base-100 px-4 py-24 transition-all duration-500 dark:bg-base-1100">
       <div className="absolute top-4 right-4">
         <DarkThemeToggle />
       </div>
-      <div className="flex flex-col justify-center gap-8">
-        <div className="text-7xl">{"Let's Chat"}</div>
-        <div className="mb-16 flex flex-row justify-center gap-8">
+  
+      <div className="flex flex-col items-center gap-15">
+        <h1 className="text-5xl font-bold text-black dark:text-base-100">Welcome to RiderChat!</h1>
+  
+        <div className="flex flex-row gap-20">
           <Link
-            className="rounded-md px-4 py-2 transition-all duration-300 hover:bg-lime-500"
-            href={"/sign-in"}
+            href="/sign-in"
+            className="w-32 text-center rounded-lg border-2 border-black bg-base-200 px-8 py-3 text-base font-semibold text-black transition-all duration-300 hover:bg-base-300 dark:border-base-100 dark:bg-base-300 dark:text-base-100 dark:hover:bg-base-350"
           >
             Login
           </Link>
           <Link
-            className="rounded-md px-4 py-2 transition-all duration-300 hover:bg-lime-500"
-            href={"/sign-up"}
+            href="/sign-up"
+            className="w-32 text-center rounded-lg border-2 border-black bg-base-200 px-8 py-3 text-base font-semibold text-black transition-all duration-300 hover:bg-base-300 dark:border-base-100 dark:bg-base-300 dark:text-base-100 dark:hover:bg-base-350"
           >
             Register
           </Link>
         </div>
       </div>
     </main>
-  );
+  );  
 }
